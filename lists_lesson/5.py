b2bfb0c96d02d00ef0c3a936a42492d03562002a
alekseik1 <1alekseik1@gmail.com>
2016-10-04 11:44:34 +0300

    5 is done

--- lists_lesson/5.py
+++ lists_lesson/5.py
@@ -1,3 +1,20 @@
 k = input().split()
-k, n = k[0], k[1]
+k, n = int(k[0]), int(k[1])
 
+a = [1]*k
+
+def fib(k, n):
+    if k >= n:
+        return 1
+    else:
+        i = 0
+        j = 0
+        while True:
+            a[i] = sum(a)
+            i += 1
+            j += 1
+            if i > len(a)-1:
+                i %= len(a)
+            if j == n: break
+    return a[0]
+print(fib(k, n))
