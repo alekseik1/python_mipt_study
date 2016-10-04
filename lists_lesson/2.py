74d2200d44d969976aee03a5ff8db68d2b0514f5
alekseik1 <1alekseik1@gmail.com>
2016-10-04 10:43:37 +0300

    2 is fully done

--- lists_lesson/2.py
+++ lists_lesson/2.py
@@ -1,10 +1,11 @@
-a = [1, 2, 2, 3, 3, 3]
+a = [1, 2, 2, 3, 3, 3, 3]
 #for i in range(1, 6): a.append(i)
 
-def one():
-    if len(a) %2 != 0: a.append(a[-1]); b = True
+def one(b):
+    if b == 1: a.append(a[-1])
     a[::2], a[1::2] = a[1::2], a[::2]
-    if b: del a[-1]
+    if b == 1: del a[-1]
+    return a
 
 def two():
     a[1::], a[0]= a[:-1:], a[-1]
@@ -13,4 +14,11 @@ def two():
 def three():
     for i in range(min(a), max(a)):
         if a.count(i) - 1 == 0: print(i)
+
+def four():
+    print(max(map(a.count, a)))
+
+print(one(len(a)%2))
+print(two())
 three()
+four()
