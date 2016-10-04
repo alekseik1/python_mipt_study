34c86a6d022d1186ce22776489531e1f06deb27d
alekseik1 <1alekseik1@gmail.com>
2016-10-04 10:22:16 +0300

    2 done

--- lists_lesson/2.py
+++ lists_lesson/2.py
@@ -1,7 +1,8 @@
 a = []
 for i in range(5): a.append(i)
-if len(a) %2 != 0:
-    a.append(a[-1]); b = True
+
+if len(a) %2 != 0: a.append(a[-1]); b = True
 a[::2], a[1::2] = a[1::2], a[::2]
 if b: del a[-1]
+
 print(a)
