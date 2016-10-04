a699cf968824756a2938f22937f16ed0f740c365
alekseik1 <1alekseik1@gmail.com>
2016-10-04 10:28:21 +0300

    2 is fully done

--- lists_lesson/2.py
+++ lists_lesson/2.py
@@ -1,8 +1,16 @@
-a = []
-for i in range(5): a.append(i)
+a = [1, 2, 2, 3, 3, 3]
+#for i in range(1, 6): a.append(i)
 
-if len(a) %2 != 0: a.append(a[-1]); b = True
-a[::2], a[1::2] = a[1::2], a[::2]
-if b: del a[-1]
+def one():
+    if len(a) %2 != 0: a.append(a[-1]); b = True
+    a[::2], a[1::2] = a[1::2], a[::2]
+    if b: del a[-1]
 
-print(a)
+def two():
+    a[1::], a[0]= a[:-1:], a[-1]
+    return a
+
+def three():
+    for i in range(min(a), max(a)):
+        if a.count(i) - 1 == 0: print(i)
+three()
