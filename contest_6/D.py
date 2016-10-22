18783010ea2e9cc996e388d2ec07485e7004f99d
alekseik1 <1alekseik1@gmail.com>
2016-10-22 21:47:02 +0300

    D is done

--- contest_6/D.py
+++ contest_6/D.py
@@ -1,40 +1,14 @@
 n = int(input())
-#n = 9
+#n = 5
 a = []
-dosh = False
-if n%2 == 0:
-    z1, z2 = n/2-1-1, n/2+1
-else:
-    z1, z2 = (n-1)/2-1, (n-1)/2+1
-if n%2 == 0:
-    for i in range(round(n/2)):
-        tmp = []
-        for j in range(n):
-            if j == z1 or j == z2:
-                tmp.append('.')
-            else:
-                tmp.append('*')
-        a.append(tmp)
-        z1 -= 1
-        z2 += 1
-    tmp = len(a)
-    for i in range(round(n/2)):
-        a.append(a[tmp-i-1])
-else:
-    for i in range(round(n/2)+1):
-        tmp = []
-        for j in range(n):
-            if j == z1 or j == z2:
-                tmp.append('.')
-            else:
-                tmp.append('*')
-        a.append(tmp)
-        z1 -= 1
-        z2 += 1
-    tmp = len(a)
-    for i in range(1, round(n/2)+1):
-        a.append(a[tmp-i-1])
-
+for i in range(n):
+    tmp = []
+    for j in range(n):
+        if i == j or n %2 == 1 and i == (n-1)/2 or n %2==1 and j == (n-1)/2 or i+j == n-1:
+            tmp.append('*')
+        else:
+            tmp.append('.')
+    a.append(tmp)
 
 for i in range(n):
     for j in range(n):
