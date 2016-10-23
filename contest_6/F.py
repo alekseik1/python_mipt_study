3ae4287d77841e508e53a238daf2dadaa2e13ad6
alekseik1 <1alekseik1@gmail.com>
2016-10-23 14:11:09 +0300

    Last one

--- contest_6/F.py
+++ contest_6/F.py
@@ -1,11 +1,21 @@
 def SwapColumns(A, a, b):
     j = len(A[0])
     i = len(A)
-    for k in range(j):
-        A[b][k] = A[k][a]
+    #ret = A[:]
+    for k in range(i):
+        #ret[k][b], ret[k][a] = A[k][a], A[k][b]
+        A[k][b], A[k][a] = A[k][a], A[k][b]
     return A
-B = SwapColumns([[2, 5], [4, 3], [2, 6]], 1, 0)
-for i in range(3):
-    for j in range(2):
+B = []
+while(True):
+    k = list(map(int, input().split()))
+    if k != []:
+        B.append(k)
+    else:
+        break
+
+B = SwapColumns(B[:-2][:-2], B[-1][0], B[-1][1])
+for i in range(len(B)):
+    for j in range(len(B[0])):
         print(B[i][j], end=' ')
     print('')
