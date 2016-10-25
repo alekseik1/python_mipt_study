97be1e56c81a66e9c5ced50dc0b6b459e7a7d5da
alekseik1 <1alekseik1@gmail.com>
2016-10-25 12:08:09 +0300

    $1 is done

--- lesson_8/6.py
+++ lesson_8/6.py
@@ -1,12 +1,24 @@
-import string
-f = open('en-ru_6.txt')
-s = f.read().split('\n')
-dic = {}
-for i in range(len(s)):
-    tmp = s[i].split('\t-\t')
-    dic[tmp[0]] = tmp[1]
-dic = {dic[a]: a for a in dic}
-for i in dic:
-    if i & string.punctuation != set():
-        i =
-print(dic)
+dict = dict()
+sl = open('en-ru_6.txt')
+slt = open('output.txt', 'w')
+s = sl.readline().rstrip()
+while len(s) > 0:
+    en_trans, ru_trans = list(s.split('\t-\t'))
+    if ',' in ru_trans:
+        for i in ru_trans.split(','):
+            i = i.lstrip()
+            if i in dict:
+                dict[i] = dict[i]+', '+en_trans
+            else:
+                dict[i] = en_trans
+    else:
+        if ru_trans in dict:
+            dict[ru_trans] = dict[ru_trans]+', '+en_trans
+        else:
+            dict[ru_trans] = en_trans
+
+    s = sl.readline().rstrip()
+
+key_sort = sorted(dict.keys())
+for i in key_sort:
+    print('\t-\t'.join((i, dict[i])), file=slt)
