4d80a5ccae531f9f124f6ddc9f19f8a818216248
alekseik1 <1alekseik1@gmail.com>
2016-10-25 10:45:01 +0300

    $1 is done

---
+++ lesson_8/4.py
@@ -0,0 +1,34 @@
+import string
+f = open('en-ru.txt')
+dic = f.read().split('\n')
+a = {}
+
+# Read dict
+for i in dic:
+    tmp = i.split('\t-\t')
+    a[tmp[0]] = tmp[1]
+
+#Read text
+f = open('input.txt')
+# Костыли из 3
+text = f.read().split()
+for i in range(len(text)):
+    k = ''
+    for j in text[i]:
+        if j not in string.punctuation:
+            k += j
+        else:
+            k += ' ' + j
+    text[i] = k
+k = ''
+for i in text:
+    k += i + ' '
+k = k.split()
+text = k
+
+# Translate text
+for i in text:
+    if i in a:
+        text[text.index(i)] = a[i]
+#print(a)
+print(*text)
