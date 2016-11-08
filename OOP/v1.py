e7bb2b6793a58200e08299448e2ff5330fe8a421
alekseik1 <1alekseik1@gmail.com>
2016-11-08 11:55:26 +0300

    v1 and v2 are done

---
+++ OOP/v1.py
@@ -0,0 +1,31 @@
+class Vector():
+    def __init__(self, s='0,0'):
+        self.x = float(s.split(',')[0])
+        self.y = float(s.split(',')[1])
+
+    def __str__(self):
+        return '[' + str(self.x) + ', ' + str(self.y) + ']'
+
+    def sum(self, sec):
+        assert isinstance(sec, Vector)
+        return Vector(str(self.x+sec.x)+','+str(self.y+sec.y))
+
+    def sc_proizv(self, b):
+        assert isinstance(b, Vector)
+        return self.x*b.x + self.y*b.y
+
+    def __abs__(self):
+        return (self.x**2+self.y**2)**0.5
+
+#a = Vector('2,5')
+#b = Vector('3,6')
+#print(a.sc_proizv(b))
+#print(a.sum(b))
+#------- 2 task --------
+N = int(input())
+A = []
+for i in range(N):
+    A.append(Vector(input()))
+for i in range(N):
+    A[i] = abs(A[i])
+print(max(A))
