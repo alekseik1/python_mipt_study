e8ff53b4007d97f3b7bbbdcdf62f77ae6fed8604
alekseik1 <1alekseik1@gmail.com>
2016-11-29 10:08:03 +0300

    3 is done

--- recursion/3.py
+++ recursion/3.py
@@ -19,22 +19,32 @@ def move_sneg(l, n):
         turtle.right(60)
         return
 
-def move(l, n):
+
+def koch(l, n):
     if n == 0:
         turtle.forward(l)
         return
 
     for i in range(n):
-        move(l/3, n-1)
+        koch(l/3, n-1)
         turtle.left(60)
-        move(l/3, n-1)
+        koch(l/3, n-1)
         turtle.right(120)
-        move(l/3, n-1)
+        koch(l/3, n-1)
         turtle.left(60)
-        move(l/3, n-1)
+        koch(l/3, n-1)
         return
 
+def move(l, n):
 
-move(1000, 5)
+    koch(l, n)
+    turtle.right(120)
+    koch(l, n)
+    turtle.right(120)
+    koch(l, n)
+    turtle.right(120)
+    return
+
+move(100, 3)
 
 #move_sneg(1000, 10)
