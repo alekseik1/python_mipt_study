ac8a730aa58db5640bebc2197257031409cfbe50
alekseik1 <alekseik1@yandex.ru>
2016-12-13 12:26:32 +0300

    ВСЕ

--- the_last/goblins.py
+++ the_last/goblins.py
@@ -1,4 +1,4 @@
-def bubble(A, B):
+def sort_with_change(A, B):
     for k in range(1, len(A)):
         for i in range(0, len(A)-k):
             if A[i] > A[i+1]:
@@ -6,16 +6,16 @@ def bubble(A, B):
                 B[i], B[i+1] = B[i+1], B[i]
                 B[i] += 1
                 B[i+1] += 1
-def bubble_without_counting(A, B):
+def sort_with_change_without_counting(A, B):
     for k in range(1, len(A)):
         for i in range(0, len(A)-k):
             if A[i] > A[i+1]:
                 A[i], A[i+1] = A[i+1], A[i]
                 B[i], B[i+1] = B[i+1], B[i]
 
-A = list(map(int, input().split(' ')))
+A = [int(x) for x in input().split()]
 B = [0]*len(A)
-bubble(A, B)
+sort_with_change(A, B)
 dic = {}
 for i in range(len(B)):
     if A[i] not in dic:
@@ -24,7 +24,7 @@ for i in range(len(B)):
         dic[A[i]] += B[i]
 keys = list(dic.keys())
 values = list(dic.values())
-bubble_without_counting(keys, values)
+sort_with_change_without_counting(keys, values)
 result = []
 for i in range(len(keys)):
     result.append(str(keys[i]) + ':' + str(values[i]))
