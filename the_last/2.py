bf1d87bbdec1087d9f4e84be17245aec34665fc4
alekseik1 <alekseik1@yandex.ru>
2016-12-13 09:55:12 +0300

    Все норм

--- the_last/2.py
+++ the_last/2.py
@@ -4,10 +4,12 @@
 a = []
 while True:
     tmp = input()
-    if tmp == '':
+    if tmp == '0 0':
         break
     a.append(list(map(int, tmp.split())))
 # Проверка пар и их вывод
-l =
+l = 0
 for i in a:
-    if i[0] % 3 != 0 and i[1]%3 != 0 and i[0]%5!=0 and i[1]%5!=0 or (i[0] > 99 and i[1] < 100 and i[1]%2==0) or (i[1] > )
+    if i[0] % 3 != 0 and i[1]%3 != 0 and i[0]%5!=0 and i[1]%5!=0 or (i[0] > 99 and i[1] < 100 and i[1]%2==0) or (i[1] > 99 and i[0] < 100 and i[0]%2==0):
+        l+=1
+print(l)
