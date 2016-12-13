67aeebd10e8e62f05671f44954faf83530025827
alekseik1 <alekseik1@yandex.ru>
2016-12-13 11:46:17 +0300

    Что-то сделал

--- the_last/brackets.py
+++ the_last/brackets.py
@@ -1,42 +1,26 @@
 class SmileChecker:
+    def __init__(self):
+        self.stack = []
 
-    s = ''
-
-    def append_smile(self, smile):
-        self.s += smile
-
-    def __get_rid_of_these_fucking_smiles(self):
-        ready = ''
-        for i in self.s:
-            if i == '[' or i == ']' or i == '{' or i == '}' or i == '(' or i == ')':
-                ready += i
-        #print(ready)
-        return ready
-
-    def brackets_check(self, s):
-        m1 = 0
-        m2 = 0
-        m3 = 0
-        for c in s:
-            if c == '(':
-                m1 += 1
-            elif c == ')':
-                m1 -= 1
-                if m1 < 0:
-                    return False
-            elif c == '[':
-                m2 += 1
-            elif c == ']':
-                m2 -= 1
-                if m2 < 0:
-                    return False
-            elif c == '{':
-                m3 += 1
-            elif c == '}':
-                m3 -= 1
-                if m3 < 0:
-                    return False
-        return m1 == 0 and m2 == 0 and m3 == 0
+    def append_smile(self, a):
+        self.stack.append(a)
 
     def check_correct(self):
-        return self.brackets_check(self.__get_rid_of_these_fucking_smiles())
+        left = [':-(', ':-[', ':-{', ':-<']
+        right = [':-)', ':-]', ':-}', ':->']
+        pair = dict(zip(right, left))
+        res = []
+        for bracket in self.stack:
+            if bracket in left:
+                res.append(bracket)
+            elif bracket in right:
+                if len(res) == 0:
+                    return False
+                if res[-1] != pair[bracket]:
+                    return False
+                else:
+                    res.pop()
+        if len(res) == 0:
+            return True
+        else:
+            return False
--- the_last/freq.py
+++ the_last/freq.py
@@ -4,24 +4,22 @@ def get_next():
         return 0
     else:
         return (get_next.seed**2 + 3*get_next.seed)%999 + 1
-
 get_next.seed = int(input())
 x = get_next()
-a = dict()
+s = {}
 while x != 0:
-    if x in a:
-        a[x] += 1
+    if x not in s:
+        s[x] = 1
     else:
-        a[x] = 0
+        s[x] += 1
     x = get_next()
-
-keys = sorted(a, key=a.get, reverse=True)
-MAX = a[keys[0]]
-answer = list()
-for i in keys:
-    if a[i] == MAX:
-        answer.append(i)
-    else:
-        break
-answer.sort()
-print(*answer)
+m = 999999999
+r = []
+for key in s:
+    if s[key]<m:
+        m = s[key]
+        r = [key]
+    elif s[key] == m:
+        r.append(key)
+r = sorted(r)
+print(*r)
