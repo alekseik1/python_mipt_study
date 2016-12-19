936a17007189bda3f7103a9d947e994b2123fd3d
alekseik1 <alekseik1@yandex.ru>
2016-12-20 00:08:43 +0300

    Кучи уже не те (видать, уборщики постарались). И опять вопрос о лисах: не верьте им, ибо их код исходит от сами-знаете-кого

--- Preparation_to_credit/heap.py
+++ Preparation_to_credit/heap.py
@@ -1,19 +1,16 @@
+import random
+
 class growing_heap:
     A = []
 
     def add(self, item):
         self.A.append(item)
-        i = len(self.A) - 1
-        while i > 1 and self.A[i//2] < item:    # Пока батя меньше своего пез... эээ, дочернего элемента
-            self.A[i] = self.A[i//2]
-            i //= 2
-            self.A[i] = item
+        self.__heapify()
 
-    def pop(self):
-        if len(self.A) == 2:
-            return self.A.pop()
-        tmp = self.A[1]
-        self.A[1] = self.A.pop()
+    def __str__(self):
+        return str(self.A)
+
+    def __heapify(self):    # Очень сильное колдунство
         i = 1
         while 2 * i + 1 < len(self.A) and self.A[i] < max(self.A[2 * i], self.A[2 * i + 1]):    # Пока не прострелили колено + пока батя меньше своих детей (то есть, надо его вниз)
             if self.A[2 * i] > self.A[2 * i + 1]:
@@ -24,4 +21,17 @@ class growing_heap:
                 i = 2 * i + 1 # Ушли уже на левого
         if 2 * i == len(self.A) - 1 and self.A[i] < self.A[2 * i]:  # Супер-костыль от Т.Ф. - отдельно рассмотреть последнюю итерацию
             self.A[i], self.A[2 * i] = self.A[2 * i], self.A[i]
+
+    def pop(self):
+        if len(self.A) == 2:
+            return self.A.pop()
+        tmp = self.A[1]
+        self.A[1] = self.A.pop()
+        self.__heapify()
         return tmp
+
+test = growing_heap()
+[test.add(random.randint(1, 100)) for x in range(100)]
+print(test)
+test.pop()
+print(test)
