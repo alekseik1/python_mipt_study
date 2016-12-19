4380bc7e9caf4cfc213c05cbb28722d242e5af7b
alekseik1 <alekseik1@yandex.ru>
2016-12-20 00:36:08 +0300

    Уборщикам повысил зарплату и они перестали домогаться до куч! Правда, кучи все еще оправляются от пережитого, но их еще чуть-чуть доработать и готово

--- Preparation_to_credit/heap.py
+++ Preparation_to_credit/heap.py
@@ -1,37 +1,54 @@
 import random
 
 class growing_heap:
-    A = []
 
-    def add(self, item):
-        self.A.append(item)
-        self.__heapify()
+    def __init__(self):
+        self.heap = []
 
     def __str__(self):
-        return str(self.A)
+        return str(self.heap)
 
-    def __heapify(self):    # Очень сильное колдунство
-        i = 1
-        while 2 * i + 1 < len(self.A) and self.A[i] < max(self.A[2 * i], self.A[2 * i + 1]):    # Пока не прострелили колено + пока батя меньше своих детей (то есть, надо его вниз)
-            if self.A[2 * i] > self.A[2 * i + 1]:
-                self.A[i], self.A[2 * i] = self.A[2 * i], self.A[i]
-                i *= 2 # Ушли ниже
-            else:
-                self.A[i], self.A[2 * i + 1] = self.A[2 * i + 1], self.A[i]
-                i = 2 * i + 1 # Ушли уже на левого
-        if 2 * i == len(self.A) - 1 and self.A[i] < self.A[2 * i]:  # Супер-костыль от Т.Ф. - отдельно рассмотреть последнюю итерацию
-            self.A[i], self.A[2 * i] = self.A[2 * i], self.A[i]
+    def add(self, item):
+        self.heap.append(item)
+        i = len(self.heap) - 1
+        parent = (i - 1) / 2
+        while i > 0 and self.heap[parent] < self.heap[i]:
+            temp = self.heap[i]
+            self.heap[i] = self.heap[parent]
+            self.heap[parent] = temp
+
+            i = parent
+            parent = (i - 1) / 2
+
+    def heapify(self, i):
+        while True:
+            leftChild = 2 * i + 1
+            rightChild = 2 * i + 2
+            largestChild = i
+            if leftChild < len(self.heap) and self.heap[leftChild] > self.heap[largestChild]:
+                largestChild = leftChild
+            if rightChild < len(self.heap) and self.heap[rightChild] > self.heap[largestChild]:
+                largestChild = rightChild
+            if largestChild == i:
+                break
+            temp = self.heap[i]
+            self.heap[i] = self.heap[largestChild]
+            self.heap[largestChild] = temp
+            i = largestChild
+
+    def buildHeap(self, A):
+        self.heap = A
+        for i in range(len(A)//2, -1, -1):
+            self.heapify(i)
 
     def pop(self):
-        if len(self.A) == 2:
-            return self.A.pop()
-        tmp = self.A[1]
-        self.A[1] = self.A.pop()
-        self.__heapify()
-        return tmp
+        res = self.heap[0]
+        self.heap = self.heap[1:]
+        self.heapify(0)
+        return res
 
-test = growing_heap()
-[test.add(random.randint(1, 100)) for x in range(100)]
-print(test)
-test.pop()
-print(test)
+heap = growing_heap()
+heap.buildHeap([0, 0, 9, 5, 23, 0, 0, 2, 2, 1, 4, 0, 12, -1, 0])
+print(heap)
+print(heap.pop())
+print(heap)
