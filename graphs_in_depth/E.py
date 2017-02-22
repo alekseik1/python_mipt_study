7f5c159ae394706bf311b174e622c391f00bc48f
alekseik1 <alekseik1@yandex.ru>
2017-02-22 14:13:29 +0300

    Тимофей - великий человек! Теперь тут есть и задача E!

--- graphs_in_depth/E.py
+++ graphs_in_depth/E.py
@@ -1,96 +1,55 @@
-class Graph():
-    graph_as_list = []
-    graph_as_list_nonstrict = []
-    graph_as_list_reverse = []
-    graph_as_matrix = []
-    dfs_used = set()
-    bad_dots = {0}
-    number_of_components = 0
-    number_of_nonstrict_components = 0
-    B = set()
-    B1 = set()
-    N, M = 0, 0
+def check(A, B, z):
+    if z in B:
+        return
+    B.add(z)
 
-    def read_graph_as_list(self):
-        self.N = int(input())
-        self.M = int(input())
-        res = [[] for i in range(self.N)]
-        for edge in range(self.M):
-            a, b = [int(x) for x in input().split()]
-            res[a].add(b)
-            res[b].add(a)
-        self.graph_as_list_nonstrict = res
-        self.graph_as_list = res
+    for element in A[z]:
+        check(A, B, element)
+def check_1(A, B, C, z):
+    if z in B or z in C:
+        return
+    C.add(z)
 
-    def kostil(self):
-        res = []
-        for i in self.graph_as_list_nonstrict:
-            res.append(list(set(i)))
-        self.graph_as_list_nonstrict = res
-
-    def read_strong_graph_as_list(self):
-        self.N = int(input())
-        self.M = int(input())
-        res = [[] for i in range(self.N)]
-        res1 = [[] for j in range(self.N)]
-        res2 = [[] for i in range(self.N)]
-        for edge in range(self.M):
-            a, b = [int(x) for x in input().split()]
-            res[a].append(b)
-            res1[b] += [a]
-            res2[a].append(b)
-            res2[b].append(a)
-        self.graph_as_list = res
-        self.graph_as_list_reverse = res1
-        self.graph_as_list_nonstrict = res2
-
-    def dfs(self, vertex, graph, used):
-        self.call_all_friends(vertex, graph, used)
-
-    def count_components(self):
-        for vertex in range(self.N):
-            if vertex not in self.dfs_used:
-                self.dfs(vertex, self.graph_as_list, self.dfs_used)
-                self.number_of_components += 1
-        return self.number_of_components
-
-    def check_strong(self, vertex=0):
-        self.call_all_friends(vertex, self.graph_as_list, self.B)
-        n = len(self.B)
-        self.call_all_friends(vertex, self.graph_as_list_reverse, self.B1)
-        k = len(self.B1)
-        if n == k:
-            return True
-        else:
-            return False
-
-    # Возвращает список, типа [0, 1] - ноль сильных и 1 слабый
-    def count_strong_components(self):
-        strong = 0
-        for vertex in range(self.N):
-            if self.check_strong(vertex):
-                strong += 1
-        return strong
-
-    def call_all_friends(self, me, friends, already_called=set()):
-        already_called.add(me)
-        for friend in friends[me]:
-            if friend not in already_called:
-                self.call_all_friends(friend, friends, already_called)
+    for element in A[z]:
+        check_1(A, B, C, element)
 
 
-A = Graph()
-A.read_strong_graph_as_list()
-A.kostil()
-n = A.count_components()
+N = int(input())
+M = int(input())
+A = [[] for i in range(N)]
+A1 = [[] for i in range(N)]
+A0 = [[] for i in range(N)]
+for j in range(M):
+    x,y = map(int, input().split())
+    A0[x] += [y]
+    A0[y] += [x]
+    A[x] +=[y]
+    A1[y] +=[x]
+B = set()
+k = 0
+s = 0
+for z in range(N):
+    if not(z in B):
 
+        check(A0, B, z)
 
-A.graph_as_list = A.graph_as_list_nonstrict
-k = A.count_components()
-
-for i in range(A.N):
-    if not A.check_strong(i):
         k += 1
-if k == 0:
-    k = 1
-print(k, n)
+        s += 1
+        if s == N:
+            break
+B = set()
+t = 0
+s = 0
+for z in range(N):
+    if not (z in B):
+        C = set()
+        D = set()
+
+        check_1(A, B, C, z)
+        check_1(A1, B, D, z)
+        t += 1
+        B = B.union(C & D)
+        s += len(C & D)
+        if s == N:
+            break
+print(k,t)
