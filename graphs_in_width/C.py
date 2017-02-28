58edff11d3955979ca5de5d614d4f69d52fca833
alekseik1 <alekseik1@yandex.ru>
2017-02-28 21:34:34 +0300

    C is done

---
+++ graphs_in_width/C.py
@@ -0,0 +1,25 @@
+def dijkstra(G, start):
+    d = {v: float('+inf') for v in G}
+    d[start] = 0
+    used = set()
+    while len(used) != len(G):
+        min_d = float('+inf')
+        for v in d:
+            if d[v] < min_d and v not in used:
+                current = v
+                min_d = d[v]
+        used.add(current)
+        for neighbour in G[current]:
+            l = d[current] + G[current][neighbour]
+            if l < d[neighbour]:
+                d[neighbour] = l
+    return d
+
+(n, m, x, y) = [int(x) for x in input().split()]
+G = {x: {} for x in range(n)}
+for i in range(m):
+    a, b = [int(x) for x in input().split()]
+    G[a][b] = 1
+    G[b][a] = 1
+res = dijkstra(G, x)
+print(res[y])
