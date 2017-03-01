d087b1153f32bbea6c988f73fa14dca5ce096dbe
alekseik1 <alekseik1@yandex.ru>
2017-03-01 13:12:33 +0300

    H is practically done

---
+++ graphs_in_width/H.py
@@ -0,0 +1,31 @@
+def dijkstra(G, start):
+    d = {v: float('+inf') for v in G}
+    d[start] = 0
+    used = set()
+    path = [[start] for x in range(len(G))]
+    k = 0
+    while len(used) != len(G):
+        min_d = float('+inf')
+        for v in d:
+            if d[v] < min_d and v not in used:
+                current = v
+                min_d = d[v]
+                path[v] += [current]
+        used.add(current)
+        for neighbour in G[current]:
+            l = d[current] + G[current][neighbour]
+            if l < d[neighbour]:
+                d[neighbour] = l
+                path[neighbour] = path[current] + [neighbour]
+    return d, path
+
+n, m, s, f = [int(x) for x in input().split()]
+G = {x: {} for x in range(n)}
+for i in range(m):
+    a, b, c = [int(x) for x in input().split()]
+    G[a][b] = c
+    G[b][a] = c
+d, path = dijkstra(G, s)
+for i in range(len(path)):
+    path[i] = list(set(path[i]))
+print(*path[f])
