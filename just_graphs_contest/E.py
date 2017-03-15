4065496987111356288351e4398fe230eb632ebc
alekseik1 <alekseik1@yandex.ru>
2017-03-15 17:18:42 +0300

    Пора путешествовать! Коммивояжер здесь.

--- just_graphs_contest/E.py
+++ just_graphs_contest/E.py
@@ -2,9 +2,9 @@ def read_graph_as_matrix():
     N, M = [int(x) for x in input().split()]
     graph = [[0]*N for i in range(N)] # матрица смежностей
     for edge in range(M):
-        a, b = [int(x) for x in input().split()]
-        graph[a][b] = 1
-        graph[b][a] = 1
+        a, b, c = [int(x) for x in input().split()]
+        graph[a][b] = c
+        graph[b][a] = c
     return graph, N
 
 A, n = read_graph_as_matrix()
@@ -15,7 +15,7 @@ Path = []
 def hamilton(curr):
     Path.append(curr)
     if len(Path) == n:
-        if A[Path[0]][Path[-1]] == 1:
+        if A[Path[0]][Path[-1]] > 0:
             return Path
         else:
             Path.pop()
@@ -23,7 +23,7 @@ def hamilton(curr):
     Visited[curr] = True
 
     for next in range(n):
-        if A[curr][next] == 1 and not Visited[next]:
+        if A[curr][next] > 0 and not Visited[next]:
             if hamilton(next):
                 return Path
     Visited[curr] = False
@@ -31,4 +31,18 @@ def hamilton(curr):
 
     return False
 
-print(*hamilton(0))
+res = {}
+
+for i in range(n):
+    tmp = hamilton(i)
+    s = 0
+    for i in range(n-1):
+        s += A[tmp[i]][tmp[i+1]]
+    s += A[Path[-1]][Path[0]]
+    res.update({s: tmp})
+    Visited = [False] * n
+    Path = []
+
+m = min(res.keys())
+print(m)
+print(*res[m])
