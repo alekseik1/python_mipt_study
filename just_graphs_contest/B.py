88b17e4c971f32e7c137ce97de2c65c00c0e30a1
alekseik1 <alekseik1@yandex.ru>
2017-03-15 14:58:02 +0300

    B is done

--- just_graphs_contest/B.py
+++ just_graphs_contest/B.py
@@ -1,37 +1,43 @@
-N, M = [int(x) for x in input().split()]
-G = {x: [] for x in range(N)}
-for i in range(M):
-    a, b = [int(x) for x in input().split()]
-    G[a].append(b)
-Edges = G
-Numbers = [-1 for x in range(N)]
-Color = [-1 for x in range(N)]
-Stack = []
+n, m = map(int, input().split())  # количество вершин и ребер в графе
+adj = [[] for i in range(n)]  # список смежности
+color = [int(0) for i in range(n)]  # массив для хранения цветов вершин
+topSort = []  # топологически упорядоченная перестановка вершин графа
 
-def topological_sort():
-    cycle = False
-    for i in range(0, N):
-        cycle = dfs(i)
-        if cycle:
+# считываем граф, заданный списком ребер
+for i in range(m):
+    v, w = map(int, input().split())
+    adj[v].append(w)
+
+
+def topologicalSort(v):  # топологическая сортировка вершин графа
+    # если вершина является черной, то не производим из нее вызов процедуры
+    if color[v] == 2:
+        return True
+    # если вершина является серой, то орграф содержит цикл, выходим из процедуры
+    if color[v] == 1:
+        return False
+    color[v] = 1  # помечаем вершину как серую
+    # запускаем обход из всех вершин, смежных с вершиной v
+    for w in adj[v]:
+        # вызов обхода от вершины w, смежной с вершиной v
+        if not topologicalSort(w):
             return False
-    for i in range(0, N):
-        Numbers[Stack.pop()] = i
+    color[v] = 2  # помечаем вершину как черную
+    # добавляем посещенную вершину в топологический порядок
+    topSort.append(v)
     return True
 
-def dfs(v):
-    if Color[v] == 1:
-        return True
-    if Color[v] == 2:
-        return False
-    Color[v] = 1
-    for i in range(len(Edges[v])):
-        if dfs(Edges[v][i]):
-            return True
-    Stack.append(v)
-    Color[v] = 2
-    return False
 
-if not topological_sort():
-    print("NO")
-    exit(0)
-print(*Numbers)
+def run():
+    cyclic = False  # флаг, показывающий содержит орграф цикл или нет
+    # запускаем процедуру, которая топологически сортирует вершины графа
+    for v in range(n):
+        if not topologicalSort(v):
+            cyclic = True
+    if not cyclic:
+        topSort.reverse()
+        # иначе выводим топологически упорядоченную перестановку его вершин
+        print(*topSort)
+    else:
+        print('NO')
+run()
