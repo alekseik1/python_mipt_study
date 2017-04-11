67ccdfb6d58062099e83e9cf68166dde653dc773
alekseik1 <alekseik1@yandex.ru>
2017-04-11 13:14:47 +0300

    Что-то еще

--- lectures/1.py
+++ lectures/1.py
@@ -3,16 +3,20 @@ def z_function_trivial(s):
     z = [0]*n
     i = 1
     while i < n:
-        while i + z[i] < n and s[z[i]] == s[i+z[i]]:
+        while i + z[i] < n and s[z[i]] == s[i+z[i]]:        # Пока мы не дошли до конца и символ на позиции z[i] равен
+            # символу на рассматриваемой позиции+z[i]
+            # (т.е., это и есть основная проверка)
             z[i] += 1
         i += 1
     return z
+
 
 def z_function(s):
     z = [0]*len(s)
     left = right = 0
     for i in range(1, len(s)):
-        x = min(z[i-left], right-i+1) if i <= right else 0
+        if i <= right:
+            x = min(z[i-left], right-i+1)
         while i+x < len(s) and s[x] == s[i+x]:
             x += 1
         if i + x - 1 > right:
