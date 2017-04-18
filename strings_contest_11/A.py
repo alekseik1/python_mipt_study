7d84eed2a0c31258ea1fc5b64d8636e80a6e7cb1
alekseik1 <alekseik1@yandex.ru>
2017-04-18 17:19:57 +0300

    A is done

---
+++ strings_contest_11/A.py
@@ -0,0 +1,12 @@
+def prefix(s):
+    v = [0]*len(s)
+    for i in range(1,len(s)):
+        k = v[i-1]
+        while k > 0 and s[k] != s[i]:
+            k = v[k-1]
+        if s[k] == s[i]:
+            k = k + 1
+        v[i] = k
+    return v
+
+print(*prefix(input()))
