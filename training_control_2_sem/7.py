95c7a95b93de358f7ce1bac77e356f687283f9f9
alekseik1 <alekseik1@yandex.ru>
2017-05-09 22:03:18 +0300

    Some is done

--- training_control_2_sem/7.py
+++ training_control_2_sem/7.py
@@ -1,35 +1,30 @@
 n, m = [int(x) for x in input().split()]
-s = {x:set() for x in range(1, n+1)}
+V = {x: [] for x in range(1, n+1)}
 for i in range(1, m+1):
     a, b = [int(x) for x in input().split()]
-    s[a].add(b)
-    s[b].add(a)
-all = [x for x in range(n)]
-left = []
-right = []
-right.append(all.pop())   # Начнем
-chelovek = all.pop()
-ex = False
-while all:
-    ex = True
-    f = -1
-    for i in s[chelovek]:
-        if i in left:   # Если недруг слева - то пометим, чтоб этого человека отправили вправо
-            f = 1
-        if f == 1 and i in right:      # Если уже пометили, чтоб отправили вправо, но и там оказался недруг, то плохи дела
-            f = -2
-            break
-    if f == -1:
-        left.append(chelovek)
-    elif f == 1:
-        right.append(chelovek)
-    chelovek = all.pop()
-if not ex:
+    V[a].append(b)
+    V[b].append(a)
+
+Color = [0] * (n + 1)
+IsBipartite = True
+
+
+def DFS(start):
+    for u in V[start]:
+        if Color[u] == 0:
+            Color[u] = 3 - Color[start]
+            DFS(u) 
+        elif Color[u] == Color[start]:
+            IsBipartite = False
+
+for i in range(1, n + 1): 
+    if Color[i] == 0: 
+        Color[i] = 1
+        DFS(i)
+if IsBipartite == True:
     print('YES')
-    print(chelovek)
-    exit(0)
-if f == -2:
-    print('NO')
+    for i in range(n+1):
+        if Color[i] == 1:
+            print(i, end=' ')
 else:
-    print('YES')
-    print(*left)
+    print('NO')
