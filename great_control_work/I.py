569879af8a6362e6ca6a8eeef1356e0f03f3c3f9
alekseik1 <alekseik1@yandex.ru>
2017-05-10 14:41:41 +0300

    I is here, but this is govnokod

--- great_control_work/I.py
+++ great_control_work/I.py
@@ -3,7 +3,7 @@ l = []
 n = len(s)
 for i in range(n):
     j = 0
-    while j+i < n and i-j >= 0 and s[i-j:i+j] == s[i-j:i+j][::-1]:
+    while j+i < n and i-j >= 0 and s[i-j:i+j+1] == s[i-j:i+j+1][::-1]:
         j += 1
     l.append(2*j - 1)
 print(*l)
