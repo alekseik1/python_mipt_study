e50a12a08d74c2125c759ad03b8c382a8610ce6f
alekseik1 <alekseik1@yandex.ru>
2017-05-10 15:27:58 +0300

    H is done, thanks to VSAlex15

--- great_control_work/H.py
+++ great_control_work/H.py
@@ -1,38 +1,17 @@
+def proverka(node=0):
+    if node == -1:
+        return
+    proverka(tree[node][1])
+    l.append(tree[node][0])
+    proverka(tree[node][2])
+    return
 n = int(input())
-l = [0]*n
+l = []
+tree = [0 for i in range(n)]
 for i in range(n):
-    l[i] = [int(x) for x in input().split()]
-
-
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.left = None
-        self.right = None
-
-
-class Tree:
-    def __init__(self):
-        self.root = None
-
-    def add(self, data):
-        if self.root is None:
-            self.root = Node(data)
-        else:
-            self._insert(self.root, data)
-
-    def _insert(self, curr, data):
-        if curr.data == data:
-            return
-        if curr.data > data:
-            next = curr.left
-            if next is None:
-                curr.left = Node(data)
-                return
-        else:
-            next = curr.right
-            if next is None:
-                curr.right = Node(data)
-                return
-        self._insert(next, data)
-
+    tree[i] = [int(x) for x in input().split()]
+proverka(0)
+if l == sorted(l):
+    print('YES')
+else:
+    print('NO')
